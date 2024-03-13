0d0a08fd9d84692715392a074779c35d85696369
Nithin1908 <76480955+ngworks1909@users.noreply.github.com>
2024-03-14 00:06:02 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// src/app.ts
+// src/app.ts  no src
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors")); // Import CORS middleware
 const app = (0, express_1.default)();
